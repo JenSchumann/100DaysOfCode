10e8621be07dddc469e64a74f4dae676c5be2afe
Jen Schumann <jenniferschumann1@gmail.com>
2018-01-10 10:52:49 -0700

    javascript practice.. everything is an object

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 It's the New Year.  My coding buddy and I are inspired to get coding & hold each other accountable.  
 
 Inspired by: http://www.100daysofcode.com/
+
+###Old Homework:
+- Basic javascript practice because we learned that "everything is an object".  This is good practice for getting back onto the coding bicycle after a holiday vacation.
--- old_homework/app.js
+++ old_homework/app.js
@@ -1,32 +1,99 @@
-console.log("day one... i can do one situp");
+// console.log("day one... i can do one situp");
 
-const orphan = {}
+/////////////////////
+//day1
+/////////////////////
 
-const dog = {
-  name: "Gizmo",
-  age: 1,
-  breed: "gremlin"
-}
+// const orphan = {}
+//
+// const dog = {
+//   name: "Gizmo",
+//   age: 1,
+//   breed: "gremlin"
+// }
 
 // console.log(dog);
 // console.log(dog.name);
 // console.log(dog.age);
 // console.log(dog.breed);
 
-const celebrity = {
-  name: "Nina Simone",
-  age: 89,
-  isCurrentlyTweeting: true
-}
+// const celebrity = {
+//   name: "Nina Simone",
+//   age: 89,
+//   isCurrentlyTweeting: true
+// }
 
 // console.log(celebrity);
 // console.log(celebrity.name);
 // console.log(celebrity.age);
 // console.log(celebrity.isCurrentlyTweeting);
 
-const TwitPolice = () => {
-  if(celebrity.isCurrentlyTweeting === true) {
-    console.log("Turn off Twitter!");
-  }
-}
-TwitPolice();
+// const TwitPolice = () => {
+//   if(celebrity.isCurrentlyTweeting === true) {
+//     console.log("Turn off Twitter!");
+//   }
+// }
+// TwitPolice();
+
+////////////////////////////
+//day 2
+///////////////////////////
+
+// const house = {
+//   doors: 9
+// }
+//
+// console.log(house);
+// house.windows = 30;
+// console.log(house);
+//
+// house.hasGarden = true;
+// console.log(house);
+
+// const bicycle = {
+//   isATricycle: false
+// }
+// console.log(bicycle);
+// bicycle.isATricycle = true;
+// console.log(bicycle);
+
+// const macros = {};
+//
+// macros.keyName = "value";
+// macros.protein = "tempeh";
+// macros.carbohydrates = "spuds";
+// macros.fats = "olive oil";
+// console.log(macros);
+
+// const guitar = {
+//   strings: 6,
+//   isAcoustic: true,
+// }
+//
+// guitar.strings = 100;
+// console.log(guitar);
+// guitar.isAcoustic = false;
+// console.log(guitar);
+// guitar.belongsTo = 'Dimebag Darrell';
+// console.log(guitar);
+// guitar.belongsTo = 'Mr. Rogers';
+// console.log(guitar);
+
+// const testObject = {};
+//
+// testObject['this is a test'] = "test";
+// console.log(testObject['this is a test']);
+//
+// testObject[2] = "I'm just messing around with objects";
+// console.log(testObject[2]);
+
+// const obj = {
+//   whatevs: "hi"
+// }
+//
+// const checkIf = () => {
+//   if(obj.whatevs == "hi") {
+//     console.log('ok');
+//   }
+// }
+// checkIf();
